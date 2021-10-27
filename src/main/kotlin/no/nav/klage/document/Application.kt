ce374b82919d74574c562a0b3166c82ea26a0f5a
Andreas Jonsson <andreas.jonsson@nav.no>
2021-10-27 21:42:15 +0200

    CORS fix

--- src/main/kotlin/no/nav/klage/document/Application.kt
+++ src/main/kotlin/no/nav/klage/document/Application.kt
@@ -4,14 +4,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
-
-
 @SpringBootApplication(exclude = [ErrorMvcAutoConfiguration::class])
-class Application
+class Application {
+
+    @Bean
+    fun corsFilter(): CorsFilter? {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowedOrigins = listOf("*")
+        config.allowedHeaders = listOf("*")
+        config.allowedMethods = listOf("GET", "POST", "PUT", "OPTIONS", "DELETE")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+
+}
 
 fun main() {
     runApplication<Application>()
