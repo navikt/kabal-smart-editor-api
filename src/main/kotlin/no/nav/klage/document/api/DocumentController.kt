e7252755a5b8075e211d6c45695b9d1214afdf69
Andreas Jonsson <andreas.jonsson@nav.no>
2021-10-27 16:04:01 +0200

    CORS test

--- src/main/kotlin/no/nav/klage/document/api/DocumentController.kt
+++ src/main/kotlin/no/nav/klage/document/api/DocumentController.kt
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
+@CrossOrigin(methods = [RequestMethod.GET, RequestMethod.DELETE, RequestMethod.HEAD, RequestMethod.POST, RequestMethod.OPTIONS, RequestMethod.PUT])
 @Api(tags = ["kabal-smart-editor-api"])
 @RequestMapping("/documents")
 class DocumentController(
@@ -31,7 +32,6 @@ class DocumentController(
         notes = "Create document"
     )
     @PostMapping("")
-    @CrossOrigin
     fun createDocument(
         @RequestBody json: String
     ): DocumentView {
@@ -44,7 +44,6 @@ class DocumentController(
         notes = "Update document"
     )
     @PutMapping("/{documentId}")
-    @CrossOrigin
     fun updateDocument(
         @PathVariable("documentId") documentId: UUID,
         @RequestBody json: String
@@ -58,7 +57,6 @@ class DocumentController(
         notes = "Get document"
     )
     @GetMapping("/{documentId}")
-    @CrossOrigin
     fun getDocument(@PathVariable("documentId") documentId: UUID): DocumentView {
         logger.debug("getDocument")
         return mapToDocumentView(documentService.getDocument(documentId))
@@ -69,7 +67,6 @@ class DocumentController(
         notes = "Create comment for a given document"
     )
     @PostMapping("/{documentId}/comments")
-    @CrossOrigin
     fun createComment(
         @PathVariable("documentId") documentId: UUID,
         @RequestBody commentInput: CommentInput
@@ -90,7 +87,6 @@ class DocumentController(
         notes = "Get all comments for a given document"
     )
     @GetMapping("/{documentId}/comments")
-    @CrossOrigin
     fun getAllCommentsWithPossibleThreads(
         @PathVariable("documentId") documentId: UUID
     ): List<CommentView> {
@@ -103,7 +99,6 @@ class DocumentController(
         notes = "Reply to a given comment"
     )
     @PostMapping("/{documentId}/comments/{commentId}/replies")
-    @CrossOrigin
     fun replyToComment(
         @PathVariable("documentId") documentId: UUID,
         @PathVariable("commentId") commentId: UUID,
@@ -126,7 +121,6 @@ class DocumentController(
         notes = "Get a given comment"
     )
     @GetMapping("/{documentId}/comments/{commentId}")
-    @CrossOrigin
     fun getCommentWithPossibleThread(
         @PathVariable("documentId") documentId: UUID,
         @PathVariable("commentId") commentId: UUID
