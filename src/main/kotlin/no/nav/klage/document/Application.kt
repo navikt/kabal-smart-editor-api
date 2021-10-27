ec41e2286843e3b9b58986f025d39a55cd38a514
Andreas Jonsson <andreas.jonsson@nav.no>
2021-10-27 16:12:07 +0200

    CORS test

--- src/main/kotlin/no/nav/klage/document/Application.kt
+++ src/main/kotlin/no/nav/klage/document/Application.kt
@@ -3,9 +3,26 @@ package no.nav.klage.document
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+
 
 @SpringBootApplication(exclude = [ErrorMvcAutoConfiguration::class])
-class Application
+class Application {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer? {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/*").allowedOrigins("*").allowedMethods("*")
+            }
+        }
+    }
+
+}
 
 fun main() {
     runApplication<Application>()
