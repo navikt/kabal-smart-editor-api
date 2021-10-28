f38a7134db5a20a660ec317bd52c060cdd0e5c09
Andreas Jonsson <andreas.jonsson@nav.no>
2021-10-28 10:54:15 +0200

    No need for CORS. Removing config.

--- src/main/kotlin/no/nav/klage/document/Application.kt
+++ src/main/kotlin/no/nav/klage/document/Application.kt
@@ -3,29 +3,10 @@ package no.nav.klage.document
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.filter.CorsFilter
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @SpringBootApplication(exclude = [ErrorMvcAutoConfiguration::class])
-class Application {
-
-    @Bean
-    fun corsFilter(): CorsFilter? {
-        val source = UrlBasedCorsConfigurationSource()
-        val config = CorsConfiguration()
-        config.allowedOrigins = listOf("*")
-        config.allowedHeaders = listOf("*")
-        config.allowedMethods = listOf("GET", "POST", "PUT", "OPTIONS", "DELETE")
-        source.registerCorsConfiguration("/**", config)
-        return CorsFilter(source)
-    }
-
-}
+class Application
 
 fun main() {
     runApplication<Application>()
