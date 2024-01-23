99d83547e450c52a21fdfa3ecf429cdc871a9b5b
Andreas Jonsson <andreas.jonsson@nav.no>
2024-01-23 12:36:40 +0100

    Delete old documents. Also include comments.
    Co-authored-by: Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>

--- src/main/resources/db/migration/V4__Delete_old_documents.sql
+++ src/main/resources/db/migration/V4__Delete_old_documents.sql
@@ -8,10 +8,22 @@ WHERE created < '2023-06-06'
     );
 
 DELETE
+FROM klage.comment
+WHERE document_id IN (SELECT id
+                      FROM klage.document
+                      WHERE created < '2023-06-06'
+                        AND id NOT IN (
+                                       '9f64b3a9-b484-48fa-8937-43e0dba24c43',
+                                       'f6e37bd8-c277-4556-8b40-7ff4ee559561',
+                                       '45038c1f-4f20-4c09-893b-c3fab817bb0a'
+                          ));
+
+DELETE
 FROM klage.document
-WHERE created < '2023-06-06'
+WHERE created
+    < '2023-06-06'
   AND id NOT IN (
-                          '9f64b3a9-b484-48fa-8937-43e0dba24c43',
-                          'f6e37bd8-c277-4556-8b40-7ff4ee559561',
-                          '45038c1f-4f20-4c09-893b-c3fab817bb0a'
+    '9f64b3a9-b484-48fa-8937-43e0dba24c43'
+    , 'f6e37bd8-c277-4556-8b40-7ff4ee559561'
+    , '45038c1f-4f20-4c09-893b-c3fab817bb0a'
     );
